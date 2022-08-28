14bbbd5d3ac687015f3b1ee1f3edaa4f3b85c670
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-08-28 15:33:00 -0500

    images added to images folder, added images as objects to galleryItems array in modules folder

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/waffle.jpg', description: 'Photo of an orange and cream-colored cat named Waffle.', likes: 0 },
+    { id: 3, path: 'images/baby_watermelon.jpg', description: 'Photo of a small watermelon growing on the vine.', likes: 0 },
+    { id: 4, path: 'images/death_canyon_shelf.jpg', description: 'Photo of the approach to Death Canyon Shelf in the Tetons.', likes: 0 },
+    { id: 5, path: 'images/edamame.jpg', description: 'Photo of a bowl of edamame.', likes: 0 },
+    { id: 6, path: 'images/melon_snack.jpg', description: 'Photo of a bowl of sliced melon next to a glass.', likes: 0 }
 ];
 
 module.exports = galleryItems;
