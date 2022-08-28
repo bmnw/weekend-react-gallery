9e3e50edf2b0e9a28dc2873984e5c0b3b17dd2e5
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-08-28 16:04:47 -0500

    within App.jsx imported useState and useEffect and axios, declared pictureList variable, created useEffect function that calls fetchPictures on page load, created working fetchPictures function to display gallery on page load with number of likes also displayed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [pictureList, setPictureList] = useState([]);
+  const [pictureLikes, setPictureLikes] = useState(0);
+
+  useEffect(() => {
+    console.log('useEffect - page load');
+    fetchPictures();
+  });
+
+    const fetchPictures = () => {
+      axios({
+        method: 'GET',
+        url: '/gallery'
+      }).then(response => {
+        setPictureList(response.data);
+      }).catch(error => {
+        console.log(error);
+        alert('Something went wrong in GET /gallery');
+      });
+    } // end fetchPictures
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+      
+        {
+        pictureList.map((picture) => {
+          return <div key={picture.id}>
+                  <img id="image" 
+                  src={picture.path} 
+                  alt={picture.description} />
+                  {picture.likes} people like this picture!
+                </div>
+        })
+        }
+
       </div>
     );
 }
