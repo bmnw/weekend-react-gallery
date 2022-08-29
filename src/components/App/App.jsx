9319a468545f4e92489ea25f7621ed47c6f4efe6
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-08-29 14:18:46 -0500

    in App.jsx added like button for each picture to the DOM, as well as a count of each picture's likes; created put request that increases the number of likes for a pic by 1 when the respective like button is clicked

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,18 +6,19 @@ import './App.css';
 function App() {
 
   const [pictureList, setPictureList] = useState([]);
-  const [pictureLikes, setPictureLikes] = useState(0);
+  const [likedPictureID, setLikedPictureID] = useState('');
 
   useEffect(() => {
-    console.log('useEffect - page load');
+    // console.log('useEffect - page load');
     fetchPictures();
-  });
+  }, []);
 
     const fetchPictures = () => {
       axios({
         method: 'GET',
         url: '/gallery'
       }).then(response => {
+        console.log(typeof response);
         setPictureList(response.data);
       }).catch(error => {
         console.log(error);
@@ -25,23 +26,46 @@ function App() {
       });
     } // end fetchPictures
 
+    // const likeSubmit = (inputID) => {
+    //   console.log('in likeSubmit, inputID', inputID);
+    //   // console.log(typeof inputID);
+    //   setLikedPictureID(inputID);
+    //   console.log('likedPictureID', likedPictureID);
+    //   // likePicture(likedPictureID);
+    // } // end likeSubmit
+
+    const likePicture = (input) => {
+      // event.preventDefault();
+      console.log('in likePicture', input);
+      axios({
+        method: 'PUT',
+        url: `/gallery/like/${input}`
+      }).then(response => {
+        fetchPictures();
+      }).catch(error => {
+        console.log(error);
+        alert('Something went wrong in POST /gallery');
+      });
+    } // end likePicture
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-      
+        
         {
-        pictureList.map((picture) => {
+        pictureList.map(picture => {
           return <div key={picture.id}>
                   <img id="image" 
                   src={picture.path} 
                   alt={picture.description} />
+                  <button value={picture.id} id="like-submit" onClick={(event) => likePicture(event.target.value)}>👍</button> 
                   {picture.likes} people like this picture!
                 </div>
         })
         }
-
+      
       </div>
     );
 }
