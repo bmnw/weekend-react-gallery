8e586d34b7fae52b61a1e9cebfcf18ab99e89816
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-08-31 10:49:49 -0500

    added conditional rendering to image div, default shows the image, when the div is clicked the image description shows, clicking the div toggles the two views

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,10 +1,21 @@
 import {useState} from 'react'; // needed for conditional rendering
 
 function GalleryItem({picture}) {
-    return <img id="image" 
-                src={picture.path} 
-                alt={picture.description}
-                />
+
+    const [showImage, setShowImage] = useState(true);
+
+    return  <div onClick={() => setShowImage(!showImage)}>
+                {
+                    showImage ? (
+                        <img id="image" 
+                        src={picture.path} 
+                        alt={picture.description}
+                        />
+                    ) : (
+                        <span>{picture.description}</span>
+                    )
+                }
+            </div>
 }
 
 export default GalleryItem;
