1c4e86f5674e5bc8bcffad0046c542b21a0c02fb
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-09-01 15:28:26 -0500

    added pool.js to modules folderand updated database name to react_gallery , installed pg, updated get and put request in gallery.router to use SQL to talk with database, added all images to database

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,6 +1,6 @@
 const galleryItems = [
     { id: 1, path: 'images/bike_ride.jpg', description: 'Selfie mid bike ride in front of the wall of a fort.', likes: 0 },
-    { id: 2, path: 'images/cake.jpg', description: 'Layered chocolate cake covered with chocolate ganache.', likes: 0 },
+    { id: 2, path: 'images/cake.jpg', description: 'Layered chocolate cake covered with dark chocolate ganache and white chocolate chips.', likes: 0 },
     { id: 3, path: 'images/edamame.jpg', description: 'A bowl of freshly harvested and cooked edamame.', likes: 0 },
     { id: 4, path: 'images/enjoying_coffee.jpg', description: 'Enjoying a flat white in the morning at a cafe.', likes: 0 },
     { id: 5, path: 'images/fjord_tour.jpg', description: 'Smiling on a boat while touring the Milford Sound in New Zealand.', likes: 0 },
--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,25 +1,39 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+// const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool.js');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
-// PUT Route
+// PUT Route using database
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
     const galleryId = req.params.id;
     console.log(galleryId);
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    const queryText = `UPDATE "gallery" SET "likes" = "likes" + 1 WHERE "id" = $1;`
+    pool.query(queryText, [galleryId])
+    .then((result) => {
+        res.sendStatus(200);
+    })
+    .catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    });
 }); // END PUT Route
 
-// GET Route
+// GET Route using database
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+    console.log('in GET /gallery');
+    const queryText = 'SELECT * FROM "gallery" ORDER BY "id";'
+    pool.query(queryText)
+    .then((result) => {
+        console.log('SELECT SUCCESS');
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log('ERROR in GET /gallery', error);
+        res.sendStatus(500);
+    });
+    // res.send(galleryItems);
 }); // END GET Route
 
 module.exports = router;
